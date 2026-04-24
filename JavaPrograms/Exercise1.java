d4a9cd4340134a42b1d7766dbda3c1fb3b1a1194
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-04-24 16:32:56 -0300

    Refactor Exercise1.java to improve code clarity
    Removed unused variables and commented code to clean up the implementation.

--- JavaPrograms/Exercise1.java
+++ JavaPrograms/Exercise1.java
@@ -3,19 +3,10 @@ import java.util.List;
 
 public class Exercise1 {
     public static void main(String []args){
-        int pessoas = 5;
-        // Criando uma lista
-        //List<Integer> alturasHomens = new ArrayList<>();
+        
         List<Integer> alturasHomens = Arrays.asList(190, 150, 176);
 
-        // Adicionar os elementos à lista
-//        alturasHomens.add(150);
-//        alturasHomens.add(175);
-//        alturasHomens.add(190);
-
-        // Criando a media
         int soma = 0;
-
 
         // Loop para percorrer cada valor e soma-lo ao próximo
         for(int altura: alturasHomens){
@@ -27,8 +18,7 @@ public class Exercise1 {
         // Mostrando o resultado
         System.out.println("A média das alturas dos homens é " + media);
 
-      //  if (alturas <= 1.45){
-           // System.out.println("Altura Inferior");
+
         }
     }
 
