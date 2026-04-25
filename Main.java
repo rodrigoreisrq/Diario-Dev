4fe82ae8108fd1ecfb9e0c9b999743b8e81dbb47
RodrigoDev12 <rodrigoreis15822@gmail.com>
2026-04-24 23:25:55 -0300

    first: pequeno projeto em Java sobre cadastro e login

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String []args){
+
+        String welcomeMessage = "Bem vindo ao app da Performance !" +
+                "Academia perfeita e acessível para você!" + "Acesse suas planilhas ou registre suas atividades.";
+        System.out.println(welcomeMessage);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual opção deseja? \n 1. Cadastrar 2.Acessar conta");
+        int escolha = scanner.nextInt();
+        scanner.nextLine();
+
+        if (escolha == 1){
+            System.out.println("Cadastre-se agora mesmo!");
+            // Cadastro
+
+
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.nextLine();
+
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua senha: ");
+            int senha = scanner.nextInt();
+
+
+            System.out.println("Digite seu email: ");
+            String email = scanner.nextLine();
+            scanner.nextLine();
+
+            // Validação
+
+            System.out.println("Cadastro concluído!");
+
+        } if(escolha == 2){
+            System.out.println("Digite seu email: ");
+            String email = scanner.nextLine();
+
+
+            System.out.println("Digite sua senha: ");
+            int senha = scanner.nextInt();
+
+            public boolean equals(Obejct obj)
+                if (email.equals(email) && senha.(senha)){
+                System.out.println("Login efetuado com sucesso!");
+                }
+
+        }
+
+
+
+
+
+    }
+
+}
